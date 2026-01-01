bb95527750efae304b2b79a01253a632342bd0e8
seemasahu12456 <seemasahu124567@gmail.com>
2026-01-02 00:53:42 +0530

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from pytubefix import YouTube
 import os
+import re
 
 app = Flask(__name__)
 CORS(app)
 
 DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+
+# Function to make valid filename
+def clean_filename(name):
+    return re.sub(r'[\\/*?:"<>|]', "", name)
 
 @app.route("/api/video-info", methods=["POST"])
 def video_info():
@@ -37,8 +43,15 @@ def download_video():
     if not stream:
         return jsonify({"error": "No stream found"}), 500
 
-    file_path = stream.download(output_path=DOWNLOAD_FOLDER)
+    filename = clean_filename(yt.title) + ".mp4"
+    file_path = os.path.join(DOWNLOAD_FOLDER, filename)
+    stream.download(output_path=DOWNLOAD_FOLDER, filename=filename)
+
     return send_file(file_path, as_attachment=True)
+
+@app.route("/")
+def home():
+    return "API is running"
 
 if __name__ == "__main__":
     app.run()
